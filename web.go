3087ac91a7ba9d582594dcaab016ae88657a5218
Stacey Brogden <toad@brothertoad.net>
2022-06-27 18:19:23 -0700

    Implement polylines in web.go.

--- web.go
+++ web.go
@@ -2,6 +2,10 @@ package main
 
 import (
   "path"
+  "strconv"
+  "strings"
+  "unicode"
+  "unicode/utf8"
   "github.com/urfave/cli/v2"
 )
 
@@ -14,7 +18,31 @@ var webCommand = cli.Command {
 func doWeb(c *cli.Context) error {
   openSvg(path.Join(config.OutputDir, "mask.svg"))
   // Need to output the actual SVG here.
-  writeSvg(`<polyline points="419,329 894,632 835,610 654,592 361,528 246,413 111,443 114,549"/>`)
+  // writeSvg(`<polyline points="419,329 894,632 835,610 654,592 361,528 246,413 111,443 114,549"/>`)
+  for _, obj := range(mask.Objects) {
+    for _, line := range(obj.Lines) {
+      // For each line, just find the point names and change them to the actual coordinates.
+      writeSvg(`<polyline points="` + substitutePoints(line) + `"/>` + "\n")
+    }
+  }
   closeSvg()
   return nil
+}
+
+func substitutePoints(s string) string {
+  var sb strings.Builder
+  words := strings.Fields(s)
+  for j, word := range(words) {
+    if j != 0 {
+      sb.WriteRune(' ')
+    }
+    rune, _ := utf8.DecodeRuneInString(word)
+    if unicode.IsDigit(rune) {
+      sb.WriteString(word)
+    } else {
+      p := mask.Points[word]
+      sb.WriteString(strconv.Itoa(p.X) + "," + strconv.Itoa(p.Y))
+    }
+  }
+  return sb.String()
 }
