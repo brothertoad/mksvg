d0beec0b4d280924397cfe59595b198e95fd4aaa
Stacey Brogden <toad@brothertoad.net>
2023-02-11 21:09:46 -0800

    v2 remove unused code

--- svg.go
+++ svg.go
@@ -8,20 +8,6 @@ import (
   "github.com/brothertoad/btu"
 )
 
-var xxxsvgPrefix =`<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="WIDTHpx" height="HEIGHTpx" viewBox="0 0 WIDTH HEIGHT" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
-  <style>
-  * {
-    fill: none;
-    stroke: STROKE-COLOR;
-    stroke-width: STROKE-WIDTH;
-  }
-  </style>
-`
-var xxxsvgSuffix = `</svg>
-`
-
 var svgPrefix =`<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg width="%dpx" height="%dpx" viewBox="0 0 %d %d" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
